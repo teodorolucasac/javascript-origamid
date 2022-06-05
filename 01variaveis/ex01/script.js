11344a659cb4ab0fcc638147d15a16b457a418e3
Lucas Teodoro <72241578+teodorolucasac@users.noreply.github.com>
2022-06-05 11:05:18 -0300

    ex01

---
+++ 01variaveis/ex01/script.js
@@ -0,0 +1,17 @@
+// Declarar uma variável com o seu nome
+var nome = 'Lucas';
+
+// Declarar uma variável com a sua idade
+var idade = 27;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Chocolate';
+
+// Declarar 5 variáveis diferentes sem valores
+var pais, estado, cidade, bairro, rua;
+
+
+console.log(nome, idade, comidaFavorita)
---
+++ 01variaveis/script.js
@@ -0,0 +1,41 @@
+var nome = 'Lucas';
+var idade = 28;
+var possuiFaculdade = true;
+
+
+console.log (nome, idade, possuiFaculdade, nome);
+
+
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco
+
+console.log (totalPreco);
+
+
+var sobrenome = 'Rafael',
+    cidade = 'Rio';
+
+console.log(sobrenome, cidade);
+
+
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoDefini);
+
+var comida;
+console.log (comida);
+comida = 'Pizza';
+console.log (comida);
+
+
+
+/* const time = 'Vasco';
+time = 'Flamengo';
+
+console.log(time);
+*/
+
