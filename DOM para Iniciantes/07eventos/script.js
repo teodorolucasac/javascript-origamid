46a4ed160e57ad447a7ab4640a1dfa01e98e1b56
Lucas Teodoro <72241578+teodorolucasac@users.noreply.github.com>
2022-06-08 17:16:59 -0300

    evento att

--- DOM para Iniciantes/07eventos/script.js
+++ DOM para Iniciantes/07eventos/script.js
@@ -1,48 +1,50 @@
-// Verifique a distância da primeira imagem
-// em relação ao topo da página
-const img = document.querySelector('img');
-const imgTop = img.offsetTop;
+// Quando o usuário clicar nos links internos do site,
+// adicione a classe ativo ao item clicado e remova dos
+// demais itens caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+const linksInternos = document.querySelectorAll('a[href^="#"]');
 
-console.log(imgTop)
-
-// Retorne a soma da largura de todas as imagens
-function somaImagens(){
-  const todasImgs = document.querySelectorAll('img');
-  let soma = 0;
-  todasImgs.forEach((imagem) => {
-    soma = soma + imagem.offsetWidth
-    console.log(imagem.offsetWidth);
+function handleLink(event) {
+event.preventDefault();
+linksInternos.forEach((link) => {
+  link.classList.remove('ativo');
 });
-  console.log(soma);
+event.currentTarget.classList.add('ativo');
 }
 
-window.onload = function(){
-  somaImagens();
+linksInternos.forEach((link) => {
+  link.addEventListener('click', handleLink);
+});
+
+
+
+
+// Selecione todos os elementos do site começando a partir do body,
+// ao clique mostre exatamente quais elementos estão sendo clicados
+const todosElementos = document.querySelectorAll('body *');
+
+function handleElemento(event) {
+  console.log(event.currentTarget);
 }
 
-// Verifique se os links da página possuem
-// o mínimo recomendado para telas utilizadas
-// com o dedo. (48px/48px de acordo com o google)
-const links = document.querySelectorAll('a');
+todosElementos.forEach((elemento) => {
+  elemento.addEventListener('click', handleElemento)
+});
 
-links.forEach((link) => {
-  const linkWidth = link.offsetWidth;
-  const linkHeight = link.offsetHeight;
-  if(linkWidth >= 48 && linkHeight >= 48) {
-    console.log(link, 'Possui acessibilidade')
-  } else {
-    console.log(link, 'Não possui boa acessibilidade')
+
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+
+//basta usar no console.log da function handleElemento o comendo: console.log(event.currentTarget.remove());
+
+// Se o usuário clicar na tecla (t), aumente todo o texto do site. 
+
+function handleClickT(event){
+  console.log(event.key);
+  if(event.key === 't'){
+    document.documentElement.classList.toggle('textomaior');
   }
-
-});
-
-
-
-// Se o browser for menor que 720px,
-// adicione a classe menu-mobile ao menu
-const browserSmall = window.matchMedia ('max-width: 720px').matches;
-
-if(browserSmall) {
-  const menu = document.querySelector('.menu');
-  menu.classList.add('menu-mobile');
 }
+
+
+window.addEventListener('keydown', handleClickT);
