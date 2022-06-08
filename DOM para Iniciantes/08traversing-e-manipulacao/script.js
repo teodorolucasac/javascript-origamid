485d409bd07b6fda8321a239804ebb9ccb2f0402
Lucas Teodoro <72241578+teodorolucasac@users.noreply.github.com>
2022-06-08 18:18:57 -0300

    travesing

--- DOM para Iniciantes/08traversing-e-manipulacao/script.js
+++ DOM para Iniciantes/08traversing-e-manipulacao/script.js
@@ -1,50 +1,24 @@
-// Quando o usuário clicar nos links internos do site,
-// adicione a classe ativo ao item clicado e remova dos
-// demais itens caso eles possuam a mesma. Previna
-// o comportamento padrão desses links
-const linksInternos = document.querySelectorAll('a[href^="#"]');
+// Duplique o menu e adicione ele em copy
+const menu = document.querySelector('.menu');
+const copy = document.querySelector('.copy');
 
-function handleLink(event) {
-event.preventDefault();
-linksInternos.forEach((link) => {
-  link.classList.remove('ativo');
-});
-event.currentTarget.classList.add('ativo');
-}
+const cloneMenu = menu.cloneNode(true);
 
-linksInternos.forEach((link) => {
-  link.addEventListener('click', handleLink);
-});
+copy.appendChild(cloneMenu);
 
 
+// Selecione o primeiro DT da dl de Faq
+const faq = document.querySelector('.faq');
+const primeiroDt = faq.querySelector('dt');
 
+console.log(primeiroDt);
 
-// Selecione todos os elementos do site começando a partir do body,
-// ao clique mostre exatamente quais elementos estão sendo clicados
-const todosElementos = document.querySelectorAll('body *');
+// Selecione o DD referente ao primeiro DT
+const proximoDD = primeiroDt.nextElementSibling;
 
-function handleElemento(event) {
-  console.log(event.currentTarget);
-}
+console.log(proximoDD);
 
-todosElementos.forEach((elemento) => {
-  elemento.addEventListener('click', handleElemento)
-});
+// Substitua o conteúdo html de .faq pelo de .animais
+const animais = document.querySelector('.animais');
 
-
-// Utilizando o código anterior, ao invés de mostrar no console,
-// remova o elemento que está sendo clicado, o método remove() remove um elemento
-
-//basta usar no console.log da function handleElemento o comendo: console.log(event.currentTarget.remove());
-
-// Se o usuário clicar na tecla (t), aumente todo o texto do site. 
-
-function handleClickT(event){
-  console.log(event.key);
-  if(event.key === 't'){
-    document.documentElement.classList.toggle('textomaior');
-  }
-}
-
-
-window.addEventListener('keydown', handleClickT);
+faq.innerHTML = animais.innerHTML
